fa239748b8b9a78ffc8ec82e347b7904e2fbeb8b
RAFAEL REIS <rafael.ls.reis@gmail.com>
2023-11-13 01:02:58 -0800

    Implemented registration using Firebase. Added minor design changes

--- app/src/main/java/com/rlsreis/grit/LoginActivity.kt
+++ app/src/main/java/com/rlsreis/grit/LoginActivity.kt
@@ -1,11 +1,116 @@
 package com.rlsreis.grit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginButton: Button
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextPassword: TextInputEditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+    private lateinit var gitLinkButton: Button
+    private lateinit var signupButton: Button
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        auth = FirebaseAuth.getInstance()
+        // Find the button or text by its ID
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        loginButton = findViewById(R.id.login_button)
+        progressBar = findViewById(R.id.progressBar2)
+        gitLinkButton = findViewById(R.id.git_link)
+        signupButton = findViewById(R.id.signup_button)
+
+
+        loginButton.setOnClickListener {
+            progressBar.visibility = View.GONE
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this@LoginActivity, "Enter Email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                Toast.makeText(this@LoginActivity, "Enter Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        // updateUI(user)
+                        reload()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Incorrect email or password",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                    }
+                }
+
+
+
+
+        }
+        // Set an OnClickListener
+        signupButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+        gitLinkButton.setOnClickListener {
+            // Define the URL you want to open
+            val url = "https://github.com/Sirraff/GRIT"
+
+            // Parse the URL and create an Intent
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
+            }
+
+            // Start the browser activity
+            startActivity(intent)
+        }
+    }
+    private fun reload() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    companion object {
+        private const val TAG = "RegisterActivity"
     }
 }
--- app/src/main/java/com/rlsreis/grit/RegisterActivity.kt
+++ app/src/main/java/com/rlsreis/grit/RegisterActivity.kt
@@ -3,19 +3,21 @@ package com.rlsreis.grit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
-
-    lateinit var editTextEmail: TextInputEditText
-    lateinit var editTextPassword: TextInputEditText
-    lateinit var buttonReg : Button
-
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextPassword: TextInputEditText
+    private lateinit var buttonReg : Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +26,16 @@ class RegisterActivity : AppCompatActivity() {
         editTextEmail = findViewById(R.id.email)
         editTextPassword = findViewById(R.id.password)
         buttonReg = findViewById(R.id.register_button)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.back_login)
+
+
+        textView.setOnClickListener {
+            finish()
+        }
 
         buttonReg.setOnClickListener {
+            progressBar.visibility = View.GONE
             val email = editTextEmail.text.toString()
             val password = editTextPassword.text.toString()
 
@@ -40,10 +50,16 @@ class RegisterActivity : AppCompatActivity() {
 
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "createUserWithEmail:success")
                         val user = auth.currentUser
+                        Toast.makeText(
+                            baseContext,
+                            "Account created!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
                         //TODO
                        // updateUI(user)
                     } else {
